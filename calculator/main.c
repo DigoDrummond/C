d6092807d5c9f9ccf2c478dfd410c4c60419aef3
DigoDrummond <rodrigodm2810@gmail.com>
2023-08-02 22:45:36 -0300

    C calculator

--- calculator/main.c
+++ calculator/main.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
+#include <math.h>
 
+float addition(int numbers[],int size);
+float subtraction(int numbers[], int size);
+float multiplication(int numbers[], int size);
+float division(int numbers[], int size);
 
 int main(){
-    printf("hello");
+    int size, choice;
+    float sum = 0.0;
+    printf("How many numbers you're gonna use? ");
+    scanf("%d", &size);
+    int numbers[size];
+    for (int i = 0 ;i < size; i++){
+        printf("\nEnter number %d: ", i+1 );
+        scanf("%d", &numbers[i]);
+    }
+    printf("<-- Menu -->\n");
+    printf("1-Add numbers.\n2-Subtract numbers.\n3-Multiply numbers.\n4-Divide numbers.5-Close program\n");
+    printf("Choose an option: ");
+    scanf("%d", &choice);
+    if(choice == 1){
+        for (int i = 0; i < size; i++)
+            sum += numbers[i];
+    }
+    if(choice == 2){
+        sum = numbers[0];
+        for (int i = 1; i < size; i++)
+            sum -= numbers[i];
+    }
+    if(choice == 3){
+        sum = 1;
+        for (int i = 0; i < size; i++)
+            sum *= numbers[i];
+    }
+    if(choice == 4){
+        sum = numbers[0];
+        for (int i = 1; i < size; i++){
+            if(numbers[i] != 0 )
+                sum /= numbers[i];
+            else{
+                printf("You can't divide a number by 0");
+                return 1;
+            }    
+        }    
+    }
+    if(choice == 5){
+        return 0;
+    }
+    if(choice < 0 || choice > 5)
+        printf("Invalid Option!");
+
+    printf("Result: %.2f\n", sum);
+
     return 0;
 }
 
