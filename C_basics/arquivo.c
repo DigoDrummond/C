ca8e21a0c80d6b4f25a71c010e09d489d5d4f26d
DigoDrummond <rodrigodm2810@gmail.com>
2023-09-04 21:38:42 -0300

    arquivo em c

--- C_basics/arquivo.c
+++ C_basics/arquivo.c
@@ -4,46 +4,59 @@
 int main()
 {
     FILE *file;
-    char filename[] = "verde.txt";
-    int n;
+    file = fopen("verde.txt", "w");
 
-    file = fopen(filename, "w");
     if (file == NULL)
     {
         return 1;
     }
 
-    
+    int n;
     scanf("%d", &n);
-
-    
+    float nums[n];
     for (int i = 0; i < n; i++)
     {
-        float numero;
-        scanf("%f", &numero);
-        fprintf(file, "%.2f", numero);
+        scanf("%f", &nums[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(file, "%.3f\n", nums[i]);
     }
 
     fclose(file);
 
-    
-    file = fopen(filename, "r");
+    file = fopen("verde.txt", "r");
+
     if (file == NULL)
     {
         return 1;
     }
 
-    fseek(file, 0, SEEK_END);      
-    long int tamanho = ftell(file); 
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    rewind(file);
 
-    for (long int i = tamanho - 1; i >= 0; i--)
+    char line[50];
+    int numsSize = 0;
+
+    while (fgets(line, sizeof(line), file))
     {
-        fseek(file, i, SEEK_SET);        
-        char linha[50];                    
-        fgets(linha, sizeof(linha), file); 
-        printf("%s", linha);               
+        float num;
+        if (sscanf(line, "%f", &num) == 1)
+        {
+            nums[numsSize++] = num;
+        }
     }
+
     fclose(file);
+
+    for (int i = numsSize - 1; i >= 0; i--)
+    {
+        if (nums[i] == (int)nums[i])
+            printf("%.0f\n", nums[i]);
+        else
+            printf("%.3f\n", nums[i]);
+    }
 
     return 0;
 }
